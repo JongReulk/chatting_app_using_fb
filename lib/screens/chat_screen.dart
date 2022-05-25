b560d0bee8bf6e8593d8c23d52d81b71003b916a
jongreulk <cheerjong12@naver.com>
2022-05-25 16:04:56 +0900

    채팅 메시지 파이어베이스에 저장하고 불러오기 및 전송가능

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,7 +1,7 @@
+import 'package:chatting_app_using_fb/chatting/chat/message.dart';
+import 'package:chatting_app_using_fb/chatting/chat/new_message.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -35,45 +35,27 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Chat screen'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              _authentication.signOut();
-            },
-            icon: Icon(Icons.exit_to_app),
-            color: Colors.white,
-          )
-        ],
-      ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('chats/obHPsuyk7kHRMyTnPmre/message')
-            .snapshots(),
-        builder: (BuildContext context,
-            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-          if(snapshot.connectionState == ConnectionState.waiting){
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          final docs = snapshot.data!.docs;
-          return ListView.builder(
-            itemCount: docs.length,
-            itemBuilder: (context, index) {
-              return Container(
-                padding: EdgeInsets.all(8),
-                child: Text(docs[index]['text'],
-                style: TextStyle(
-                  fontSize: 20
-                ),),
-              );
-            },
-          );
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Chat screen'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                _authentication.signOut();
+              },
+              icon: Icon(Icons.exit_to_app),
+              color: Colors.white,
+            )
+          ],
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Expanded(
+                child: Messages(),
+              ),
+              NewMessage(),
+            ],
+          ),
+        ));
   }
 }
